4f6af1f96f0e4ac03d2e4375b62eadb5b23089c9
zhaopengn <zhaopengn@gmail.com>
2015-01-10 22:29:04 +0800

    beat test

--- spec/player_spec.js
+++ spec/player_spec.js
@@ -4,8 +4,20 @@ var m = require('jsmockito').JsMockito;
 
 describe("player", function(){
     it("game spec", function(){
-        var mocked_console = m.spy(console);
-        console.log("李四被打败了");
-        m.verify(mocked_console).log("李四被打败了.");
+
+        //var mocked_console = m.spy(console);
+        //
+        //mocked_console.log("李四被打败了.");
+        //
+        //m.verify(mocked_console).log("李四被打败了.");
+
+        var zhang=new fighter('张三',100,20);
+        var li=new fighter('李四',100,20);
+        var mocked=m.spy(console);
+        beats(zhang,li,mocked);
+        m.verify(mocked).log("李四被打败了");
+
     });
 });
+
+
