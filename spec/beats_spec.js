4ecc4b7ebc3e9b5304fb9081d1f6e5cfe07332a3
zhaopengn <zhaopengn@gmail.com>
2015-01-11 19:24:15 +0800

    new test :fighter(with weapon) beats fighter(without defense); && passed it

--- spec/beats_spec.js
+++ spec/beats_spec.js
@@ -94,7 +94,18 @@ describe("player Fighter VS Human", function(){
 
         var exp="战士张三用优质木棒攻击了战士李四,李四受到了7点伤害,李四剩余生命：8";
         expect(s).toBe(exp);
+    });
 
+    it("geme spec - fighter(with weapon) beats fighter(without defense)",function(){
+        var woodBar = new Weapon("优质木棒",3),
+            zhang = new Fighter("战士","张三",20,6,woodBar),
+            helmet = new Defense("头盔",2),
+            li = new Fighter("战士","李四",15,7,woodBar,helmet);
+
+        var s=li.beats(zhang);
+
+        var exp="战士李四用优质木棒攻击了战士张三,张三受到了10点伤害,张三剩余生命：10";
+        expect(s).toBe(exp);
     });
 
     xit("game spec - a fight",function(){
